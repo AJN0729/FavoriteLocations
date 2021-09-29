99d53ea141d8d9648051190e8b9c074b31c17e85
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-09-29 10:11:20 -0500

    main screen working navigation view working

--- FavoriteLocations/View/ContentView.swift
+++ FavoriteLocations/View/ContentView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FavoritesView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct FavoritesView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FavoritesView()
     }
 }
--- FavoriteLocations/View/FavoritesView.swift
+++ FavoriteLocations/View/FavoritesView.swift
@@ -27,12 +27,26 @@ struct FavoritesView: View {
                         .bold()
                         .kerning(1.0)
                         .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Text("share your location.")
+                        .font(.title2)
+                        .kerning(1.0)
+                        .multilineTextAlignment(.center)
                         .padding(.bottom, 200)
+                    
+                    Text("Begin Exploring")
+                        .padding()
+                        .font(.system(size: 28, weight: .semibold))
+                        .frame(width: 250, height: 50)
+                        .background(Color.white)
+                        .foregroundColor(.green)
+                        .cornerRadius(12.0)
                 }
                 .foregroundColor(.white)
                 .padding(.bottom, 300)
                 .background(Color.green)
-//                .edgesIgnoringSafeArea(.all)
+                .edgesIgnoringSafeArea(.all)
             }
         }
     }
