966acf22b56870b79f526f6707505c9868c09558
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-09-29 08:24:21 -0500

    Initial Commit

---
+++ FavoriteLocations/FavoriteLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteLocationsApp.swift
+//  FavoriteLocations
+//
+//  Created by AMStudent on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
